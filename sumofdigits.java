f3a4412194e8f8870f41692b4aa1fc97a1ac3c02
revati12m-debug <revati12m@gmail.com>
2026-05-07 13:56:24 +0530

    Add files via upload

---
+++ sumofdigits.java
@@ -0,0 +1,16 @@
+package palindrome;
+
+public class sumofdigits {
+public static void main(String args[])
+{
+	int num=123456789;
+	int sum=0;
+	
+	while(num>0) 
+	{
+		sum+=num%10;
+		num/=10;
+	}
+	System.out.println("sum="+sum);
+}
+}
