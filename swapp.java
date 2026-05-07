0a820015a115688cd83cbc519752e556ce003329
revati12m-debug <revati12m@gmail.com>
2026-05-07 12:04:20 +0530

    Add files via upload

---
+++ swapp.java
@@ -0,0 +1,18 @@
+package palindrome;
+
+public class swapp
+{
+	public static void main (String args[])
+	{
+	int a=5;
+	int b=10;
+	
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	
+	System.out.println("a="+a);
+	System.out.println("b="+b);
+}
+
+}
