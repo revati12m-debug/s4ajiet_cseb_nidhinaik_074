0860e1a15c926a6f2bd950ff1007883fc4bb8323
revati12m-debug <revati12m@gmail.com>
2026-05-07 11:56:15 +0530

    Add files via upload

---
+++ rev.java
@@ -0,0 +1,17 @@
+package palindrome;
+
+public class rev {
+public class divide
+{
+	public static void main(String args[])
+	{
+		int a=16;
+		int b=8;
+		int c=a/b;
+		int d=a%b;
+		System.out.println("Quoitient"+c);
+		System.out.println("remainder"+d);
+	}
+}
+
+}
