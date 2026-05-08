1e7d879fb4d94e9798e08b17cffb78d1c0f3801e
revati12m-debug <revati12m@gmail.com>
2026-05-08 10:39:52 +0530

    Add files via upload

---
+++ anagram.java
@@ -0,0 +1,30 @@
+package palindrome;
+
+import java.util.Arrays;
+
+public class anagram {
+public static void main (String args[])
+{
+	String a="heart";
+	String b="earth";
+	
+	if(a.length()==b.length())
+	{
+		char arr1[]=a.toCharArray();
+		char arr2[]=b.toCharArray();
+		
+		Arrays.sort(arr1);
+		Arrays.sort(arr2);
+		
+		if (Arrays.equals(arr1,arr2))
+		{
+		System.out.println("its is anagram");
+	}
+	else
+	{
+		System.out.println("not an anagram");
+	}
+}
+}
+}
+
